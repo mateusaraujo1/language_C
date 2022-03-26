803e2e11aad5b9abdcf2ddbf24bffc4b8507b7b3
Mateus <f.mateusaraujo20@gmail.com>
2022-03-26 16:26:00 -0300

    add ex08

--- exercicios/aula02/ex08.c
+++ exercicios/aula02/ex08.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
 int main() {
+    int N, cont, fatorial = 1;
+    float E = 1;
+
+    printf("Digite um valor: ");
+    scanf("%d", &N);
+
+    for (cont = 1; cont <= N; cont++) {
+
+        fatorial = fatorial * cont;
+        E += 1.0 / fatorial;
+
+    }
+
+    printf("%.2f", E);
+
     return 0;
 }
